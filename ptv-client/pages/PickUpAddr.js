b68cc7ad75ba6db15cb5ca1477f0d1870d00a1f5
Tim Kluser <tim.kluser@gmail.com>
2022-03-24 13:45:36 +0100

    added Time with mui

--- ptv-client/pages/PickUpAddr.js
+++ ptv-client/pages/PickUpAddr.js
@@ -1,58 +1,84 @@
-import React, {Component} from 'react'
+import React, {useState, useContext} from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
-// import TimePicker from '@mui/lab/TimePicker';
+import TimePicker from '@mui/lab/TimePicker';
+
 
 const PickUpAddr = () =>{
+
+    const [pickTimeStart, setPickTimeStart] = useState("")
+    const [pickTimeEnd, setPickTimeEnd] = useState("")
     return (
         <div>
-        <Grid container spacing={1}>
-            <Grid item xs = {12}>
-                <Typography variant='h4'>PickUp-Addr</Typography>
+            <Grid container spacing={1}>
+                <Grid item xs = {12}>
+                    <Typography variant='h4'>PickUp-Addr</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Address Line 1"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Address Line 2"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="City"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="State"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="ZIP"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                <TimePicker
+                            label="PickupTime Start"
+                            value={pickTimeStart}
+                            onChange={(newValue) => {
+                            setPickTimeStart(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                </Grid>
+                {/* <Grid item xs = {1}>
+                    <Typography variant='h5'>to</Typography>
+                </Grid> */}
+                <Grid item xs = {6}>
+                    <TimePicker
+                            label="PickupTime End"
+                            value={pickTimeEnd}
+                            onChange={(newValue) => {
+                            setPickTimeEnd(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                </Grid>
             </Grid>
-            <Grid item xs={8}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Address Line 1"
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={8}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Address Line 2"
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={8}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="City"
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={6}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="State"
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={6}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="ZIP"
-                    fullWidth
-                />
-            </Grid>
-            
-        </Grid>
         </div>
     )
 }
--- ptv-client/pages/_app.js
+++ ptv-client/pages/_app.js
@@ -2,13 +2,17 @@ import '../styles/globals.css'
 import PackageForm from './PackageForm'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 function MyApp({ Component, pageProps }) {
+  
   return(
     <Container maxWidth="sm">
       <Typography variant="h2">Delivery Service</Typography>
       <PackageForm/>
     </Container>
+    
   )
 }
 
